a708aa5093c83f2480f7ed41df63c248238bb803
DingDechao <dingdechao_0486@163.com>
2020-12-11 21:38:45 +0800

    init springboot-maven project

---
+++ springboot-maven/src/main/java/com/ddc/projects/springboot/springbootmaven/SpringbootMavenApplication.java
@@ -0,0 +1,13 @@
+package com.ddc.projects.springboot.springbootmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMavenApplication.class, args);
+	}
+
+}
---
+++ springboot-maven/src/test/java/com/ddc/projects/springboot/springbootmaven/SpringbootMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddc.projects.springboot.springbootmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
