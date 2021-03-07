e45ae6b38055e477c6eae99dc05fd297ba916614
DingDechao <dingdechao_0486@163.com>
2021-03-07 23:38:44 +0800

    Get properties from application.properties

--- springboot-maven/src/main/java/com/ddc/projects/springboot/springbootmaven/controller/HelloWorldController.java
+++ springboot-maven/src/main/java/com/ddc/projects/springboot/springbootmaven/controller/HelloWorldController.java
@@ -1,13 +1,23 @@
 package com.ddc.projects.springboot.springbootmaven.controller;
 
+import com.ddc.projects.springboot.springbootmaven.properties.DdcProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+    @Autowired
+    private DdcProperties ddcProperties;
+
     @GetMapping("/helloworld")
     public String helloRest() {
         return "hello world";
+    }
+
+    @GetMapping("/ddcname")
+    public String ddcName() {
+        return ddcProperties.getName();
     }
 }
