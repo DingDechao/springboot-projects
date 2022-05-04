78dd61fdc8409e870a4a32762af3e78dd5df7c7f
DingDechao <dingdechao_0486@163.com>
2022-05-04 13:22:24 +0800

    backup

--- springboot-maven/springboot-maven-core-service/src/main/java/com/ddc/projects/springboot/springbootmaven/springbootmavencoreservice/entity/User.java
+++ springboot-maven/springboot-maven-core-service/src/main/java/com/ddc/projects/springboot/springbootmaven/springbootmavencoreservice/entity/User.java
@@ -1,15 +1,26 @@
 package com.ddc.projects.springboot.springbootmaven.springbootmavencoreservice.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 @Data
 public class User {
 
-    private String id;
+    // mp 自动找id， 如果不是id需要主动申明主键
+    // @TableId
+    private Long id;
+    //@TableField("mp_name")
     private String name;
     private String age;
     private String email;
     private String managerId;
     private String createTime;
+    // 数据库没有对应的字段
+    // 1. transient 但是不参与序列化
+    // 2. static 但是是类级不好
+    // 3. @TableField(exist = false)
+    @TableField(exist = false)
+    private String remark;
 
 }
--- springboot-maven/springboot-maven-core-service/src/test/java/com/ddc/projects/springboot/springbootmaven/springbootmavencoreservice/mapper/UserMapperTest.java
+++ springboot-maven/springboot-maven-core-service/src/test/java/com/ddc/projects/springboot/springbootmaven/springbootmavencoreservice/mapper/UserMapperTest.java
@@ -1,16 +1,20 @@
 package com.ddc.projects.springboot.springbootmaven.springbootmavencoreservice.mapper;
 
 import com.ddc.projects.springboot.springbootmaven.springbootmavencoreservice.entity.User;
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Handler;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 @SpringBootTest
 public class UserMapperTest {
@@ -26,4 +30,49 @@ public class UserMapperTest {
         logger.debug("UserMapperTest>>>>>testGetAllUser User list size {}", userList.size());
         assertTrue(userList.size() > 0);
     }
+
+    @Test
+    @Ignore
+    public void testInsertOneUser() {
+        User user = new User();
+        user.setName("zhangsan");
+        user.setAge("31");
+        user.setManagerId("1088248166370832385");
+        user.setCreateTime("2022-05-03 08:35:22");
+        int rows = userMapper.insert(user);
+        logger.debug("UserMapperTest>>>>>testInsertOneUser Insert {} user", rows);
+        assertTrue(rows == 1);
+    }
+
+    @Test
+    public void testSelectById() {
+        User user = userMapper.selectById(1087982257332887553l);
+        assertNotNull(user);
+        assertEquals("big boss", user.getName());
+    }
+
+    @Test
+    public void testSelectIds() {
+        List<User> userList = userMapper.selectBatchIds(Arrays.asList(1087982257332887553l,1088248166370832385l));
+        assertNotNull(userList);
+        assertTrue(userList.size() == 2);
+
+        List<User> emptyUserList = userMapper.selectBatchIds(Arrays.asList(123456l));
+        assertNotNull(userList);
+        assertTrue(userList.size() == 2);
+    }
+
+    @Test
+    public void selectByMap() {
+        //map.put("column name1", value)
+        //map.put("column name2", value)
+        //where column 1 = value and column2 = value
+        Map<String, Object> columnMap = new HashMap<>();
+        columnMap.put("manager_id", "1087982257332887553");
+        List<User> userList = userMapper.selectByMap(columnMap);
+        assertNotNull(userList);
+        assertTrue(userList.size() >= 1);
+    }
+
+    
 }
